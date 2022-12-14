7e5c041b47bad7b4fac618ee2497410c8bf87cea
narimiran <narimiran@disroot.org>
2022-12-14 20:07:50 +0100

    day14.clj: better algorithm for part 2
    ~20x speedup!

--- clojure/day14.clj
+++ clojure/day14.clj
@@ -2,13 +2,21 @@
   (:require aoc))
 
 
+(def start-x 500)
+
+
 (defn path->rock [[ax ay] [bx by]]
   (for [x (range (min ax bx) (inc (max ax bx)))
         y (range (min ay by) (inc (max ay by)))]
     [x y]))
 
-(defn pour-sand [settled]
-  (loop [[x y] [500 0]]
+(defn add-floor [rocks]
+  (let [floor-y (+ 2 (apply max (map second rocks)))
+        floor (for [x (range (- start-x floor-y) (+ start-x floor-y 1))] [x floor-y])]
+    (apply conj rocks floor)))
+
+(defn drop-grain [settled]
+  (loop [[x y] [start-x 0]]
     (let [down  [x       (inc y)]
           left  [(dec x) (inc y)]
           right [(inc x) (inc y)]]
@@ -18,17 +26,27 @@
         (not (settled right)) (recur right)
         :else [x y]))))
 
-(defn simulate [rocks part]
-  (let [start      500
-        floor      (+ 2 (apply max (map second rocks)))
-        floor-pts  (for [x (range (- start floor) (+ start floor 1))] [x floor])
-        rocks      (apply merge rocks floor-pts)
-        count-sand (fn [settled] (- (count settled) (count rocks) (- 1 part)))]
+(defn fill-to-full [settled [x y]]
+  (if (settled [x y]) settled
+      (-> settled
+          (fill-to-full [x       (inc y)])
+          (fill-to-full [(dec x) (inc y)])
+          (fill-to-full [(inc x) (inc y)])
+          (conj [x y]))))
+
+(defn part-1 [rocks]
+  (let [floor (apply max (map second rocks))]
     (loop [settled rocks]
-      (let [[x y] (pour-sand settled)]
-        (if (= y (if (= part 1) (dec floor) 0))
-          (count-sand settled)
+      (let [[x y] (drop-grain settled)]
+        (if (= y (dec floor))
+          (- (count settled) (count rocks))
           (recur (conj settled [x y])))))))
+
+(defn part-2 [rocks]
+  (-> rocks
+      (fill-to-full [start-x 0])
+      count
+      (- (count rocks))))
 
 
 (def rocks
@@ -40,7 +58,8 @@
        (apply concat)
        (map #(apply path->rock %))
        (apply concat)
-       set))
+       set
+       add-floor))
 
-[(simulate rocks 1)
- (simulate rocks 2)]
+[(part-1 rocks)
+ (part-2 rocks)]
