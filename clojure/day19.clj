b710abef7d415498b447c2310b90db26c92f4bec
narimiran <narimiran@disroot.org>
2023-01-13 09:39:12 +0100

    day19.clj: make it 8x faster
    The most of the speedup comes from calculating a hash
    instead of having a *nested* vector `[t bots resources]`
    as an entry for `seen` set.
    This set can now be `dense-int-set`, which also brings improvement.
    
    Each other improvement is relatively minor, but all together
    they bring the total execution time under 600 ms.
    (It was 4.7 seconds)
    
    Big thanks to @joinr for the review, tips and general advice!

--- clojure/day19.clj
+++ clojure/day19.clj
@@ -1,5 +1,6 @@
 (ns day19
-  (:require aoc))
+  (:require aoc
+            [clojure.data.int-map :refer [dense-int-set]]))
 
 
 (defrecord Blueprint [prices max-resources])
@@ -22,9 +23,37 @@
      (/ (* time (inc time)) 2)))
 
 (defn build-bot  [resources price]
-  (reduce-kv (fn [rscs k v] (update rscs k #(- % v)))
-             resources
-             price))
+  (reduce-kv
+   (fn [rscs rsc cost]
+     (assoc rscs rsc (- (resources rsc) cost)))
+   resources
+   price))
+
+(defn no-negatives? [resources]
+  (reduce-kv
+   (fn [positive _ v]
+     (if (neg? (long v))
+       (reduced false)
+       positive))
+   true
+   resources))
+
+
+(defn calc-hash [t bots resources]
+  ;; "Hashing" function to avoid having a nested vector
+  ;; as a hash-map key.
+  ;;
+  ;; Very fragile! Don't touch the coefficients,
+  ;; i.e. I need a more robust way of doing this.
+  (+ ^long t
+     (* 33 (+ (* 2 ^long (:ore bots))
+              (* 5 ^long (:clay bots))
+              (* 13 ^long (:obs bots))
+              (* 37 ^long (:geode bots))))
+     (* 1151 (+ (* 3 ^long (:ore resources))
+                (* 5 ^long (:clay resources))
+                (* 11 ^long (:obs resources))
+                (* 31 ^long (:geode resources))))))
 
 (defn geodes [t bp]
   (let [types [:ore :clay :obs :geode]
@@ -36,46 +65,50 @@
         {:keys [prices max-resources]} bp
         ,
         collect (fn [resources bots t]
-                  (into {} (for [res types]
-                             [res (min (* (max-resources res) t)
-                                       (+ (resources res) (bots res)))])))]
-    (loop [stack [state]
-           seen #{}
+                  (->> resources
+                       (reduce-kv (fn [acc res v]
+                                    (assoc! acc res
+                                            (min (* (max-resources res) t)
+                                                 (+ v (bots res)))))
+                                  (transient resources))
+                       persistent!))]
+    (loop [stack (list state)
+           seen (transient (dense-int-set))
            score 0]
-      (if (empty? stack) score
-          (let [{:keys [t bots resources skipped]} (peek stack)
-                state-hash [t bots resources]
-                stack' (pop stack)]
-            (if (or (zero? t)
-                    (seen state-hash)
-                    (< (best-case (resources :geode) (bots :geode) t) score))
-              (recur stack' (conj seen state-hash) (max score (resources :geode)))
-              (let [[stack'' skipped]
-                    (reduce (fn [[stack can-build] bot]
-                              (let [rscs (build-bot resources (prices bot))]
-                                (if (and (not (skipped bot))
-                                         (> t (times bot))
-                                         (not-any? neg? (vals rscs))
-                                         (< (bots bot) (max-resources bot))
-                                         (< (resources bot) (* 1.5 (max-resources bot))))
-                                  [(conj stack {:t (dec t) , :resources (collect rscs bots t)
-                                                :bots (update bots bot inc) , :skipped #{}})
-                                   (conj can-build bot)]
-                                  [stack can-build])))
-                            [stack' #{}]
-                            types)
-                    ,
-                    stack
-                    (if (or (< (resources :ore) (max-resources :ore))
-                            (and (pos? (bots :clay))
-                                 (< (resources :clay) (max-resources :clay)))
-                            (and (pos? (bots :obs))
-                                 (< (resources :obs) (max-resources :obs))))
-                      (conj stack'' {:t (dec t) , :resources (collect resources bots t)
-                                     :bots bots , :skipped skipped})
-                      stack'')]
-                (recur stack (conj seen state-hash) score))))))))
-
+      (if-let [state (first stack)]
+        (let [{:keys [t bots resources skipped]} state
+              state-hash (calc-hash t bots resources)
+              stack' (rest stack)]
+          (if (or (zero? t)
+                  (seen state-hash)
+                  (< (best-case (resources :geode) (bots :geode) t) score))
+            (recur stack' (conj! seen state-hash) (max score ^long (resources :geode)))
+            (let [[stack'' skipped]
+                  (reduce (fn [[stack can-build] bot]
+                            (let [rscs (build-bot resources (prices bot))]
+                              (if (and (not (skipped bot))
+                                       (> t (times bot))
+                                       (no-negatives? rscs)
+                                       (< (bots bot) (max-resources bot))
+                                       (< (resources bot) (* 1.5 (max-resources bot))))
+                                [(conj stack {:t (dec t) , :resources (collect rscs bots t)
+                                              :bots (update bots bot inc) , :skipped #{}})
+                                 (conj can-build bot)]
+                                [stack can-build])))
+                          [stack' #{}]
+                          types)
+                  ,
+                  stack
+                  (if (or (< (resources :ore) (max-resources :ore))
+                          (and (pos? (bots :clay))
+                               (< (resources :clay) (max-resources :clay)))
+                          (and (pos? (bots :obs))
+                               (< (resources :obs) (max-resources :obs))))
+                    (conj stack'' {:t (dec t) , :resources (collect resources bots t)
+                                   :bots bots , :skipped skipped})
+                    stack'')]
+              (recur stack (conj! seen state-hash) score))))
+        score))))
 
 (defn part-1 [blueprints]
   (->> blueprints
