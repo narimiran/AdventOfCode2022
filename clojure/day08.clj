efbfe15fb296cf5bae9b5c4497aed2b539d6de0e
narimiran <narimiran@disroot.org>
2023-01-14 09:16:43 +0100

    day08.clj: 20% faster

--- clojure/day08.clj
+++ clojure/day08.clj
@@ -2,8 +2,14 @@
   (:require aoc))
 
 
-(defn visible-from-outside? [lower-than-current? direction]
-  (every? lower-than-current? direction))
+(defn visible-from-outside? [lower-than-current? directions]
+  (reduce
+   (fn [acc dir]
+     (if (every? lower-than-current? dir)
+       (reduced true)
+       acc))
+   false
+   directions))
 
 (defn viewing-distance [lower-than-current? direction]
   (->> direction
@@ -15,17 +21,15 @@
 (defn go-through-forrest [height-map]
   (let [hor  height-map
         vert (aoc/transpose height-map)]
-    (for [[y row] (map-indexed vector hor)
-          [x col] (map-indexed vector vert)
+    (for [[^long y row] (map-indexed vector hor)
+          [^long x col] (map-indexed vector vert)
           :let [dirs [(rseq (subvec row 0 x)) ; left
                       (subvec row (inc x))    ; right
                       (rseq (subvec col 0 y)) ; up
                       (subvec col (inc y))]   ; down
-                is-lower? (partial > (row x))
-                f (fn [agg func] (agg #(func is-lower? %) dirs))
-                visible-directions  (f filter visible-from-outside?)
-                visible-trees-count (f map viewing-distance)]]
-      {:is-visible?  (not-empty visible-directions)
+                lower-than-current? #(< ^long % ^long (row x))
+                visible-trees-count (map #(viewing-distance lower-than-current? %) dirs)]]
+      {:is-visible?  (visible-from-outside? lower-than-current? dirs)
        :scenic-score (reduce * visible-trees-count)})))
 
 (defn solve
