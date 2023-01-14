dbd0219a7d31ed1def394b3133ce6512b6f9cdeb
narimiran <narimiran@disroot.org>
2023-01-14 10:41:28 +0100

    day17.clj: 20% faster

--- clojure/day17.clj
+++ clojure/day17.clj
@@ -2,52 +2,63 @@
   (:require aoc))
 
 
-(def rocks
-  [[[0 0] [1 0] [2 0] [3 0]]
-   [[1 0] [0 1] [1 1] [2 1] [1 2]]
-   [[0 0] [1 0] [2 0] [2 1] [2 2]]
-   [[0 0] [0 1] [0 2] [0 3]]
-   [[0 0] [1 0] [0 1] [1 1]]])
+(defrecord Point [x y])
 
-(defn move-left  [[x y]] [(dec x) y])
-(defn move-right [[x y]] [(inc x) y])
-(defn move-down  [[x y]] [x (dec y)])
+
+(defmacro ->Rock [pts]
+  `(map (fn [[x# y#]] (->Point x# y#)) ~pts))
+
+(def rocks
+  [(->Rock [[0 0] [1 0] [2 0] [3 0]])
+   (->Rock [[1 0] [0 1] [1 1] [2 1] [1 2]])
+   (->Rock [[0 0] [1 0] [2 0] [2 1] [2 2]])
+   (->Rock [[0 0] [0 1] [0 2] [0 3]])
+   (->Rock [[0 0] [1 0] [0 1] [1 1]])])
+
+(defn move-left  [{:keys [x y]}] (->Point (dec x) y))
+(defn move-right [{:keys [x y]}] (->Point (inc x) y))
+(defn move-down  [{:keys [x y]}] (->Point x (dec y)))
+
+(defn pt+ [a b]
+  (->Point (+ (:x a) (:x b)) (+ (:y a) (:y b))))
 
 (defn inbounds? [rock]
-  (every? #(<= 0 % 6) (map first rock)))
+  (every? #(<= 0 % 6) (map :x rock)))
 
-(defn peaks [tower max-y]
-  (set (for [[x y] tower
-             :when (= y max-y)]
-         x)))
-
+(defn peaks-hash [tower max-y]
+  (reduce + (for [{:keys [x y]} tower
+                 :when (= y max-y)]
+             (* x x))))
 
 (defn play [movements rounds]
   (let [M (count movements)
         R (count rocks)
-        tower (set (for [x (range 7)] [x 0]))]
-    (loop [[r m max-y skipped seen tower] [0 0 0 0 {} tower]]
+        tower (set (for [x (range 7)] (->Point x 0)))]
+    (loop [[r m max-y skipped seen tower]
+           [0 0 0 0 (transient {}) tower]]
       (if (>= r rounds) (+ max-y skipped)
-          (let [init-pos [2 (+ max-y 4)]
-                rock     (map #(aoc/pt+ init-pos %) (nth rocks (mod r R)))]
+          (let [init-pos (->Point 2 (+ max-y 4))
+                rock     (mapv (partial pt+ init-pos) (nth rocks (mod r R)))]
             (recur
              (loop [rock rock
-                    m m]
+                    m    (long m)]
                (let [move-fn    (if (= \< (nth movements (mod m M)))
                                   move-left move-right)
-                     rock'      (map move-fn rock)
+                     rock'      (mapv move-fn rock)
                      moved-rock (if (and (inbounds? rock')
                                          (not-any? tower rock'))
                                   rock' rock)
-                     rock'' (map move-down moved-rock)]
+                     rock'' (mapv move-down moved-rock)]
                  (if (not-any? tower rock'')
                    (recur rock'' (inc m))
-                   (let [max-y       (reduce max max-y (map second moved-rock))
+                   (let [max-y       (max max-y (:y (peek moved-rock)))
                          tower       (->> tower
-                                          (filter #(> (second %) (- max-y 100)))
+                                          (filter #(> (:y %) (- max-y 100)))
                                           (concat moved-rock)
                                           set)
-                         t-hash      [(peaks tower max-y) (mod r R) (mod m M)]
+                         t-hash      (+ ^long (peaks-hash tower max-y)
+                                        ^long (* 100 (mod r R))
+                                        ^long (* 500 (mod m M)))
                          r           (inc r)
                          [r skipped] (if-let [[r1 y1] (seen t-hash)]
                                        (let [dr (- r r1)
@@ -56,7 +67,7 @@
                                          [(+ r (* skipped-periods dr))
                                           (+ skipped (* skipped-periods dy))])
                                        [r skipped])]
-                     [r (inc m) max-y skipped (assoc seen t-hash [r max-y]) tower]))))))))))
+                     [r (inc m) max-y skipped (assoc! seen t-hash [r max-y]) tower]))))))))))
 
 
 (defn solve
