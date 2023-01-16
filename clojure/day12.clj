65242db17ced8ba9629446979d9abf1de142781a
narimiran <narimiran@disroot.org>
2023-01-16 10:45:19 +0100

    day12.clj: 3x faster
    Same old:
    Convert 2d vector to int, and use `int-map` to give it 2x boost.
    
    After that, several minor tweaks to make it even faster.

--- clojure/day12.clj
+++ clojure/day12.clj
@@ -1,6 +1,14 @@
 (ns day12
-  (:require aoc))
+  (:require aoc
+            [clojure.data.int-map :refer [int-map]]))
 
+
+(def ^:const N 100)
+
+
+(defn neighbours ^longs [^long pt]
+  (for [^long delta [-1 1 (- N) N]]
+    (+ delta pt)))
 
 (defn find-val [grid height]
   (some #(when (= height (val %)) (key %)) grid))
@@ -8,35 +16,37 @@
 (defn travel [grid part]
   (let [start (find-val grid -14)
         end   (find-val grid -28)
-        grid  (merge grid {start 0 end 25})]
-    (loop [seen      #{end}
-           [hd & tl] [[0 end]]]
-      (let [[steps curr] hd]
-        (if (and (= (grid curr) 0)
+        grid  (-> grid
+                  (assoc start 0)
+                  (assoc end 25))]
+    (loop [seen  #{end}
+           queue [[0 end]]]
+      (let [[steps curr] (peek queue)
+            h (grid curr)]
+        (if (and (zero? h)
                  (or (= part 2) (= curr start)))
           steps
           (let [nbs (filter #(and (not (seen %))
-                                  (>= (grid % -99)
-                                      (dec (grid curr))))
-                            (aoc/neighbours curr 4))
-                nexts (map #(vector (inc steps) %) nbs)]
+                                  (>= (grid % -99) (dec h)))
+                            (neighbours curr))
+                nexts (mapv #(vector (inc steps) %) nbs)]
             (recur (into seen nbs)
-                   (into (vec tl) nexts))))))))
+                   (into nexts (pop queue)))))))))
 
-(defn parse-line [line]
-  (mapv #(- (int %) (int \a)) line))
-
-(defn parse-input [input]
-  (->> (aoc/read-input input :vector)
-       (mapv parse-line)
-       aoc/vec2d->grid))
+(defn make-grid [lines]
+  (into (int-map)
+        (for [[y line] (map-indexed vector lines)
+              [x char] (map-indexed vector line)]
+          [(+ x (* N y))
+           (- (int char) (int \a))])))
 
 (defn solve
   ([] (solve 12))
   ([input]
-   (let [grid (parse-input input)]
-     [(travel grid 1)
-      (travel grid 2)])))
+   (let [grid (make-grid (aoc/read-input input :vector))
+         p1 (future (travel grid 1))
+         p2 (future (travel grid 2))]
+     [@p1 @p2])))
 
 
 (solve)
